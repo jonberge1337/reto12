cfe8caf6fcb51cf6f5b9eef3fceed5b53f1824f9
jonberge1337 <jonbergerandi@gmail.com>
2019-02-07 14:22:11 +0100

    Javadoc completado

--- src/reto12/Reto12.java
+++ src/reto12/Reto12.java
@@ -11,9 +11,11 @@ import javax.swing.JOptionPane;
 public class Reto12 {
 
 	/**
-	 * @param menu Array de 4, las opciones iniciales
+	 * Este método rellena el Array del menú principal
+	 * 
+	 * @param String[] menu
 	 */
-	public static void crearArrayMenuPrincipal(String[] menu) {
+	public static void rellenarArrayMenuPrincipal(String[] menu) {
 
 		menu[0] = "Ciudades";
 		menu[1] = "Restaurantes";
@@ -21,20 +23,23 @@ public class Reto12 {
 		menu[3] = "Conversión";
 	}
 	/**
+	 * Este método rellena el Array del menú "Conversor"
 	 * 
-	 * @param menu Array de 4, opciones del conversor
+	 * @param String[] menu
 	 */
-	public static void crearArrayMenuConversor(String[] menu) {
+	public static void rellenarArrayMenuConversor(String[] menu) {
 
-		menu[0] = "Dolares a Baht";
-		menu[1] = "Baht a Dolares";
+		menu[0] = "Dólares a Baht";
+		menu[1] = "Baht a Dólares";
 		menu[2] = "Euros a Baht";
 		menu[3] = "Baht a Euros";
 	}
 	/**
-	 * @param ciudades Array de 10 para guardar las ciudades de Tailandia
+	 * Este método rellena el Array del menú "Ciudades"
+	 * 
+	 * @param String[] ciudades
 	 */
-	public static void crearCiudades(String[] ciudades) {
+	public static void rellenarCiudades(String[] ciudades) {
 
 		ciudades[0] = "Bangkok";
 		ciudades[1] = "Chiang Mai";
@@ -49,9 +54,11 @@ public class Reto12 {
 
 	}
 	/**
-	 * @param habitantes Array de 10, se guardan el numero de habitantes por ciudad
+	 * Este método rellena el Array de las poblaciones
+	 * 
+	 * @param int[] habitantes
 	 */
-	public static void crearPoblacion(int[] habitantes) {
+	public static void rellenarPoblacion(int[] habitantes) {
 
 		habitantes[0] = 5696409;
 		habitantes[1] = 132556;
@@ -66,9 +73,11 @@ public class Reto12 {
 
 	}
 	/**
-	 * @param restaurantes Array bidi de 10x3, se guardan 3 restaurantes por cada ciudad
+	 * Este método rellena el Array de los restaurantes por cada ciudad
+	 * 
+	 * @param String[][] restaurantes
 	 */
-	public static void crearRestaurantes(String[][] restaurantes) {
+	public static void rellenarRestaurantes(String[][] restaurantes) {
 
 		restaurantes[0][0] = "Tealicious";
 		restaurantes[0][1] = "Pagoda Chinese restaurant";
@@ -79,16 +88,16 @@ public class Reto12 {
 		restaurantes[1][2] = "Italics restaurant";
 
 		restaurantes[2][0] = "Barrab restaurant"; 
-		restaurantes[2][1] = "Hungry wolf�s restaurant";
+		restaurantes[2][1] = "Hungry wolf's restaurant";
 		restaurantes[2][2] = "Heaven burger";
 
 		restaurantes[3][0] = "Sandwich me";
 		restaurantes[3][1] = "Gecko cabane restaurant";
-		restaurantes[3][2] = "Jenna�s Bistro & wine";
+		restaurantes[3][2] = "Jenna's Bistro & wine";
 
 		restaurantes[4][0] = "Siam supper club"; 
 		restaurantes[4][1] = "Dee Plee";
-		restaurantes[4][2] = "Sam�s steaks and Grill";
+		restaurantes[4][2] = "Sam's steaks and Grill";
 
 		restaurantes[5][0] = "Cafe des Amis Fine Dining";
 		restaurantes[5][1] = "Caprice Restaurant & Bar";
@@ -112,9 +121,11 @@ public class Reto12 {
 
 	}
 	/**
-	 * @param precios Array de 10x3, se guardan los precios por restaurante
+	 * Este método rellena el Array de los precios por cada restaurante
+	 * 
+	 * @param int[][] precios
 	 */
-	public static void crearPrecios(int[][] precios){
+	public static void rellenarPrecios(int[][] precios){
 
 		precios[0][0] = 360;
 		precios[0][1] = 480;
@@ -189,75 +200,81 @@ public class Reto12 {
 		precios[9][2] = 486;
 	}
 	/**
-	 * @param curiosidades Array de 10, se guardan 10 curiosidades sobre Tailandia
+	 * Este método rellena el Array de las curiosidades sobre Tailandia
+	 * 
+	 * @param String[] curiosidades
 	 */
-	public static void crearCuriosidades(String[] curiosidades) {
+	public static void rellenarCuriosidades(String[] curiosidades) {
 
-		curiosidades[0] = "1. El rey de Tailandia Bhumibol Adulyadej fue el monarca m�s longevo de la historia (M�s de 70 a�os).";
+		curiosidades[0] = "1. El rey de Tailandia Bhumibol Adulyadej fue el monarca más longevo de la historia (Más de 70 años).";
 		curiosidades[1] = "2. Hasta 1939 Tailandia se llamaba Siam.";
-		curiosidades[2] = "3. En Tailandia est� prohibido pisar billetes y monedas.";
+		curiosidades[2] = "3. En Tailandia está prohibido pisar billetes y monedas.";
 		curiosidades[3] = "4. En los parques cada tarde a les 18h suena el himno de Tailandia y todo el mundo se queda parado.";
-		curiosidades[4] = "5. Tailandia ha sido muchos a�os el primer exportador de arroz en el mundo.";
-		curiosidades[5] = "6. Est�ticamente a los Tailandeses les gusta la piel blanca (como a la mayor�a de pa�ses del este de Asia). As� que nada de ir a la playa a ponerse moreno.";
-		curiosidades[6] = "7. La sociedad tailandesa es una de las m�s tolerantes del mundo, pues hay muchas Lady Boys.";
-		curiosidades[7] = "8. Al despertarse, es com�n en los pueblos que la gente salga a barrer las veredas o las playas.";
+		curiosidades[4] = "5. Tailandia ha sido muchos años el primer exportador de arroz en el mundo.";
+		curiosidades[5] = "6. Estéticamente a los Tailandeses les gusta la piel blanca (como a la mayoría de países del este de Asia). Así que nada de ir a la playa a ponerse moreno.";
+		curiosidades[6] = "7. La sociedad tailandesa es una de las más tolerantes del mundo, pues hay muchas Lady Boys.";
+		curiosidades[7] = "8. Al despertarse, es común en los pueblos que la gente salga a barrer las veredas o las playas.";
 		curiosidades[8] = "9. Los perros de Tailandia pueden tener seguro de vida.";
 		curiosidades[9] = "10. Frank Cuesta (Frank de la Jungla) vive en Tailandia.";
 
 	}
 	/**
-	 * @param opciones Array de 3, opciones para mostrar las curiosidades
+	 * Este método rellena el Array del submenú principal
+	 * 
+	 * @param String[] menu
 	 */
-	public static void crearArraySubmenuPrincipal(String[] opciones) {
-		
+	public static void rellenarArraySubmenuPrincipal(String[] opciones) {
+
 		opciones[0] = "Lista completa";
-		opciones[1] = "aleatoria";
+		opciones[1] = "Aleatoria";
 		opciones[2] = "Especifica";
 
 	}
-	
-	/**
-	 * @param afirmacion Array de 2, para dar opcion a repetir el programa
-	 */
-	public static void crearArrayAfirmacion(String[] afirmacion) {
 
-		afirmacion[0] = "Si";
+	/**
+	 * Este método rellena el Array para repetir el proceso
+	 * 
+	 * @param String[] afirmacion
+	 */
+	public static void rellenarArrayAfirmacion(String[] afirmacion) {
+
+		afirmacion[0] = "Sí";
 		afirmacion[1] = "No";
 
 	}
 
-	//===============================================================
-	//				CREAR LOS DIFERENTES ARRAYS;
-	//===============================================================
 
-	//===============================================================
-	//				LAS OPCIONES DEL CONVERSOR
-	//===============================================================
 	/**
-	 * @param valor Variable double para guardar el numero entero que ha introducido el usuario
-	 * @return cambio Devuelve el valor tras la operacion aritmetica
+	 * Este método convierte de Dólares a Baht
+	 * 
+	 * @param double valor la cifra que deseamos convertir
+	 * @return cambio devuelve la cifra con la conversión realizada
 	 */
 	public static String dolaresBaht(double valor) {
 		String cambio;
 		String formato =String.format("%.2f", valor*31.51);
-		cambio = valor + " Dolares en Baths es "+formato;
+		cambio = valor + " Dólares en Baths es "+formato;
 
 		return cambio;
 	}
 	/**
-	 * @param valor Variable double para guardar el numero entero que ha introducido el usuario
-	 * @return cambio Devuelve el valor tras la operacion aritmetica
+	 * Este método convierte de Baht a Dólares
+	 * 
+	 * @param double valor la cifra que deseamos convertir
+	 * @return valor devuelve la cifra con la conversión realizada
 	 */
 	public static String bahtDolares(double valor) {
 		String cambio;
 		String formato =String.format("%.2f", valor/31.51);
-		cambio = valor + " Baht en Dolares es "+formato;
+		cambio = valor + " Baht en Dólares es "+formato;
 
 		return cambio;
 	}
 	/**
-	 * @param valor Variable double para guardar el número entero que ha introducido el usuario
-	 * @return cambio Devuelve el valor tras la operación aritmética
+	 * Este método convierte de Euros a Baht
+	 * 
+	 * @param double valor la cifra que deseamos convertir
+	 * @return cambio devuelve la cifra con la conversión realizada
 	 */
 	public static String eurosBaht(double valor) {
 		String cambio;
@@ -267,8 +284,10 @@ public class Reto12 {
 		return cambio;
 	}
 	/**
-	 * @param valor Variable double para guardar el número entero que ha introducido el usuario
-	 * @return cambio Devuelve el valor tras la operación aritmética
+	 * Este método convierte de Baht a Euros
+	 * 
+	 * @param double valor la cifra que deseamos convertir
+	 * @return cambio devuelve la cifra con la conversión realizada
 	 */
 	public static String bahtEuros(double valor) {
 		String cambio;
@@ -278,15 +297,13 @@ public class Reto12 {
 		return cambio;
 	}
 
-	//===============================================================
-	//				LAS OPCIONES DEL CONVERSOR;
-	//============��===================================================
-
 
 	/**
+	 * Este método muestra una ventana emergente con los valores transferidos
+	 * 
 	 * @param opciones Array de tamaño variable que devuelve unas opciones para el usuario
 	 * @param pregunta String que contiene una pregunta para mostrar en el panel
-	 * @return opcion Devuelve el valor entero segun lo que haya elegido el usuario
+	 * @return opcion Devuelve el valor entero según lo que haya elegido el usuario
 	 */
 	public static int menuJoption(String[] opciones, String pregunta) {
 
@@ -298,8 +315,12 @@ public class Reto12 {
 
 		return opcion;
 	}
-
-	public static void opcionEspecifica(String[] curiosidades) {
+	/**
+	 * Este método da a elegir una posición del Array
+	 * 
+	 * @param String[] curiosidades la cifra que deseamos convertir
+	 */
+	public static void opcionEspecifica(String[] array) {
 
 		Scanner sc=new Scanner(System.in);
 		int eleccion;
@@ -307,37 +328,54 @@ public class Reto12 {
 
 		do {
 			try {
-				eleccion=Integer.parseInt(JOptionPane.showInputDialog(null,"Teclea un n�mero del 1 al 10: "));
+				eleccion=Integer.parseInt(JOptionPane.showInputDialog(null,"Teclea un número del 1 al 10: "));
 			} catch (Exception e) {
-				System.err.println("introduce un número");
+				System.err.println("Introduce un número");
 			}
 
 		}while (eleccion<1 || eleccion>10);
 
-		JOptionPane.showMessageDialog(null,curiosidades[eleccion-1]);
+		mostrarStrings(array[eleccion-1]);
 		sc.close();
 
 	}
-
-	public static void opcionAleatorio(String[] curiosidades) {
+	/**
+	 * Este método muestra una posición aleatoria del array
+	 * 
+	 * @param String[] array la cifra que deseamos convertir
+	 */
+	public static void opcionAleatorio(String[] array) {
 
 		int aleat;
 		aleat=(int)(Math.random()*10);
 
-		JOptionPane.showMessageDialog(null,curiosidades[aleat]);
+		mostrarStrings(array[aleat]);
 
 	}
+	/**
+	 * Este método rellena los porcentajes dentro del array "arrayAlmacenamiento"
+	 * 
+	 * @param int total contiene la población total de Tailandia
+	 * @param String[] ciudades contiene las ciudades insertadas
+	 * @param int dividendo transmite el número de habitantes de la ciudad
+	 * @param String[] arrayAlmacenamiento almacena el String generado
+	 */
+	public static void arrayParaMostrarPorcentajes(int total, String ciudades[], int dividendo[], String[] arrayAlmacenamiento) {
 
-	public static void arrayParaMostrarPorcentajes(int total, String ciudades[], int divisor[], String[] arrayAlmacenamiento) {
-		// Nos calcula el porcentaje de habitantes que tiene cada pueblo y nos la muestra
-
-		for (int i = 0; i < divisor.length; i++) {
-			String formato = String.format("%.2f", (float)100 * divisor[i] / total);
+		for (int i = 0; i < dividendo.length; i++) {
+			String formato = String.format("%.2f", (float)100 * dividendo[i] / total);
 			arrayAlmacenamiento[i] = "En " + ciudades[i] + " viven el "+ formato + "% de Tailandia";
 		}
 
 	}
-
+	/**
+	 * Este método rellena un String para mostrar los precios posteriormente en un JOption
+	 * 
+	 * @param int[][] precios los precios por cada restaurante
+	 * @param String[][] restau los restaurantes insertados
+	 * @param String[] ciudades las ciudades de Tailandia
+	 * @param String[] arrayAlmacenamiento almacena el String generado
+	 */
 	public static void arrayParaMostrarPrecios(int[][] precios, String[][] restau, String[] ciudades, String[] arrayAlmacenamiento) {
 
 		for (int i = 0; i < restau.length; i++) {
@@ -353,21 +391,33 @@ public class Reto12 {
 
 	}
 
-
+	/**
+	 * Este método muestra los arrays unidimensionales
+	 * 
+	 * @param array el array que queremos mostrar
+	 */
 	public static void mostrarArraysUni(String[] array) {
 
 		JOptionPane.showMessageDialog(null,array);
 	}
 
-	public static void mostrarArraysBi(String[][] array) {
-
-		JOptionPane.showMessageDialog(null,array);
-	}
-
+	/**
+	 * Este método muestra los Strings
+	 * 
+	 * @param frase el String que queremos mostrar
+	 */
 	public static void mostrarStrings(String frase) {
 		JOptionPane.showMessageDialog(null,frase);
 	}
-
+	/**
+	 * Este método muestra el menú y nos pide elegir una opción
+	 * 
+	 * @param String[] array
+	 * @param String[] botones
+	 * @param String pregunta
+	 * @param String[] afirma
+	 * @param String repeticion
+	 */
 	public static void submenuPrincipal(String[] array, String[] botones, String pregunta, String[] afirma, String repeticion) {
 
 		int panel;
@@ -383,13 +433,21 @@ public class Reto12 {
 			}else if (panel==2){
 				opcionEspecifica(array);
 			}else{ 
-				mostrarStrings("Programa finalizado");
+				mostrarStrings("Programa finalizado.");
 				System.exit(panel);
 			}
 			repetir = menuJoption(afirma, repeticion) ;
 		}while(repetir == 0);
 	}
-
+	/**
+	 * Este método muestra el menú del Conversor
+	 * 
+	 * @param String[] array
+	 * @param String[] botones
+	 * @param String pregunta
+	 * @param String[] afirma
+	 * @param String repeticion
+	 */
 	public static void menuConversor(String[] opciones, String pregunta, String[] afirma, String repeticion) {
 		float valor;
 		int panel;
@@ -407,7 +465,7 @@ public class Reto12 {
 				}catch (Exception e) {
 
 					System.err.println("Introduce un número");
-					
+
 				}
 			}while(valor<=0);
 			panel = menuJoption(opciones, pregunta);
@@ -428,21 +486,37 @@ public class Reto12 {
 		}while(repetir == 0);
 		sc.close();
 	}
-
+	
+	/**
+	 * Este método llama a los método para rellenar cada array
+	 * 
+	 * @param String[] afirmacion
+	 * @param String[] principal
+	 * @param String[] conversor
+	 * @param String[] opcionesCuriosidades
+	 * @param String[] ciudades
+	 * @param int[] habitantes
+	 * @param String[][] restaurantes
+	 * @param int[][] precios
+	 * @param String[] curiosidades
+	 * @param String[] arrayAlmacenCiudades
+	 * @param String[] arrayAlmacenRestaurantes
+	 * @param int poblacionTotal
+	 */
 	public static void rellenarArrays(String[] afirmacion, String[] principal,
 			String[] conversor, String[] opcionesCuriosidades, String[] ciudades,
 			int[] habitantes, String[][] restaurantes, int[][] precios, String[] curiosidades,
 			String[] arrayAlmacenCiudades,String[] arrayAlmacenRestaurantes,int poblacionTotal) {
 
-		crearArrayAfirmacion(afirmacion);
-		crearArrayMenuPrincipal(principal);
-		crearArrayMenuConversor(conversor);
-		crearArraySubmenuPrincipal(opcionesCuriosidades); 
-		crearCiudades(ciudades);
-		crearPoblacion(habitantes);
-		crearRestaurantes(restaurantes);
-		crearPrecios(precios);
-		crearCuriosidades(curiosidades);
+		rellenarArrayAfirmacion(afirmacion);
+		rellenarArrayMenuPrincipal(principal);
+		rellenarArrayMenuConversor(conversor);
+		rellenarArraySubmenuPrincipal(opcionesCuriosidades); 
+		rellenarCiudades(ciudades);
+		rellenarPoblacion(habitantes);
+		rellenarRestaurantes(restaurantes);
+		rellenarPrecios(precios);
+		rellenarCuriosidades(curiosidades);
 		arrayParaMostrarPorcentajes(poblacionTotal, ciudades, habitantes, arrayAlmacenCiudades);
 		arrayParaMostrarPrecios(precios, restaurantes, ciudades, arrayAlmacenRestaurantes);
 	}
@@ -456,7 +530,7 @@ public class Reto12 {
 		int repetir;
 		String pregunta;
 		String repeticion;
-		pregunta = "¿Qué opción quiere elegir?";
+		pregunta = "¿Qué opción desea elegir?";
 		repeticion = "¿Desea volver a repetir?";
 
 		String[] afirmacion = new String[2];
